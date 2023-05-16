b1ca8fc074f08c24d1b9012c771945b7f0c205f7
Zhao KK <kikakkz@hotmail.com>
2023-05-16 10:20:42 +0800

    Add announcement bar

--- src/components/announcement/Announcement.vue
+++ src/components/announcement/Announcement.vue
@@ -1,14 +1,24 @@
 <template>
   <div class='announcement color-main'>
-    For test announcement
+    <q-img :src='logoImg' class='logo' fit='contain' />
+    <span>TODO: A brown fox jump over a white rabbit!</span>
   </div>
 </template>
+
+<script setup lang='ts'>
+import logoImg from '../../assets/Logo.svg'
+</script>
 
 <style lang='sass' scoped>
 .announcement
   background: #D4FFE8
-  min-height: 48px
   width: 100%
   text-align: center
-  line-height: 48px
+  font-size: 14px
+  padding: 5px 0
+
+.logo
+  height: 26px
+  width: 18px
+  margin: 0 6px 0 0
 </style>
