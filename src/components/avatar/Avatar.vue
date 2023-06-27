10716a490436c849d2bfca093303b30edebb0e45
Zhao KK <kikakkz@hotmail.com>
2023-06-27 16:37:47 +0800

    Add personal info

--- src/components/avatar/Avatar.vue
+++ src/components/avatar/Avatar.vue
@@ -6,38 +6,43 @@
     icon='account_circle'
     size='16px'
   >
-    <q-menu :offset='[120, 22]'>
+    <q-menu :offset='[200, 22]'>
       <q-list class='text-primary' :style='{minWidth: "240px"}'>
         <q-item>
           <q-space />
-          <q-icon name='account_circle' size='80px' />
+          <q-icon name='account_circle' size='60px' />
           <q-space />
         </q-item>
-        <q-item v-if='_user.User?.EmailAddress?.length'>
+        <q-item v-if='_user.User?.EmailAddress?.length && !_viewerAddress?.length'>
           <q-space />
-          <span>{{ _user.User?.EmailAddress }}</span>
+          <span :style='{color: "#3DBB77"}'>{{ _user.User?.EmailAddress }}</span>
           <q-space />
         </q-item>
         <q-item v-if='_viewerAddress?.length'>
           <q-space />
           <span
-            :style='{whiteSpace: "nowrap", overflow: "hidden", textOverflow: "ellipsis"}'
+            :style='{whiteSpace: "nowrap", overflow: "hidden", textOverflow: "ellipsis", color: "#3DBB77"}'
           >
             {{ _viewerAddress }}
           </span>
           <q-space />
         </q-item>
-        <q-item v-if='!_user.User?.EmailAddress?.length && _viewerAddress?.length'>
-          <div class='row' :style='{width: "100%"}'>
-            <q-space />
-            <q-btn flat class='btn btn-small btn-main' @click='onBindEmailAddressClick'>
+        <q-separator />
+        <q-item>
+          <q-icon name='email' size='28px' />
+          <div :style='{fontSize: "14px", lineHeight: "28px"}'>
+            {{ _user.User?.EmailAddress?.length ? _user.User?.EmailAddress : ' - ' }}
+          </div>
+          <q-space />
+          <div class='row cursor-pointer' @click='onBindEmailAddressClick'>
+            <div :style='{fontSize: "14px", lineHeight: "28px", color: "#3DBB77"}'>
               {{ $t('MSG_BIND_EMAIL_ADDRESS') }}
-            </q-btn>
-            <q-space />
+            </div>
+            <q-icon name='chevron_right' :style='{color: "#3DBB77", marginTop: "4px"}' size='20px' />
           </div>
         </q-item>
+        <q-separator />
         <q-item>
-          <q-space />
           <q-icon name='logout' size='24px' :style='{marginRight: "10px"}' />
           <span>{{ $t('MSG_LOGOUT') }}</span>
           <q-space />
