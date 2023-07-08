c6b55d3dca23b2c72ee72e16023325219eafd297
Zhao KK <kikakkz@hotmail.com>
2023-07-08 17:09:12 +0800

    Use right api

--- src/boot/axios.ts
+++ src/boot/axios.ts
@@ -15,7 +15,14 @@ declare module '@vue/runtime-core' {
 // good idea to move this instance creation inside of the
 // "export default () => {}" function below (which runs individually
 // for each client)
-const api = createAPI('/api', AppID) as AxiosInstance
+let baseURL = window.location.protocol + '//api.' + window.location.hostname + '/api'
+if (window.location.hostname.startsWith('www.')) {
+  baseURL = window.location.origin.replace('www', 'api') + '/api'
+}
+if (window.location.hostname.includes('.npool.top')) {
+  baseURL = window.location.protocol + '//api.npool.top' + (window.location.port.length ? ':' + window.location.port : '') + '/api'
+}
+const api = createAPI(baseURL, AppID) as AxiosInstance
 
 // define common response handle
 interface CommonError {
